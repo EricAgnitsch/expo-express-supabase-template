0fb7a241ce97763f303a79a3a9ba986b604e356e
Eric Agnitsch <ericagnitsch.dev@gmail.com>
2024-02-28 02:32:16 -0500

    Setup nativewind and tested with app.tsx

---
+++ template/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
+    './App.tsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
