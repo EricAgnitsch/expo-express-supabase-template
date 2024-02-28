c54980a769dd350a9d63e6dcd6f6dcae699367f9
Eric Agnitsch <ericagnitsch.dev@gmail.com>
2024-02-28 02:45:51 -0500

    Added expo router and moved app to use page layout pattern

--- template/app/index.tsx
+++ template/app/index.tsx
@@ -1,7 +1,7 @@
 import {StatusBar} from 'expo-status-bar';
 import {Text, View} from 'react-native';
 
-export default function App() {
+export default function AppPage() {
   return (
     <View className={'flex-1 items-center justify-center'}>
       <Text>Open up App.tsx to start working on your app!</Text>
